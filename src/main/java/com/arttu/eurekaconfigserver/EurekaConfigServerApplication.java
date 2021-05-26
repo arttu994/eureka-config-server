8f8a53a8cf484252b5bf1f289aec9395fb961c86
Artur Shaiekhov <arttu9994@gmail.com>
2021-05-26 21:05:11 +0300

    Initial eureka config server

---
+++ src/main/java/com/arttu/eurekaconfigserver/EurekaConfigServerApplication.java
@@ -0,0 +1,13 @@
+package com.arttu.eurekaconfigserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EurekaConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaConfigServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/arttu/eurekaconfigserver/EurekaConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.arttu.eurekaconfigserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
